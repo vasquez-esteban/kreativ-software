0b86cb69d948516de3cc1388843e8684454205d7
vasquez-esteban <evasquezp@unal.edu.co>
2024-05-25 15:31:19 -0500

    v.0.1 - Init fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://master--kreativ-software.netlify.app",
+  site: "https://master--kreatif-software.netlify.app",
   integrations: [tailwind({ configFile: "./tailwind.config.mjs" })],
 });
